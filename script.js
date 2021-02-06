c8885984cfeae093e10366a71b1dbf273cf38d70
linyuxuanlin <824676271@qq.com>
2021-02-06 16:20:20 +0800

    up

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 new Docute({
   target: '#docute',
   sourcePath: './docs/',
-  nav: [{
+  nav: [
+    {
       title: 'Home',
       link: '/'
     },
@@ -34,85 +35,37 @@ new Docute({
       ]
     }
     */
-    {
-      title: '电路设计',
-      links: [
+   {
+    title: '电路设计',
+    links: [
 
-        {
-          link: 'AirForce-充满灵性的电机驱动模块',
-        },
-        {
-          link: '如何设计一款单片机的最小系统',
-        },
-        {
-          link: 'SwiftCtrl-蓝牙手柄',
-        },
-        {
-          link: 'TinyDVR-小巧身材，满载动力',
-        },
-        {
-          link: 'ZenDriver-高性能的电机驱动',
-        },
-        {
-          link: 'PCB布线规范',
-        },
-        {
-          link: 'PCB元件布局规范',
-        },
-        {
-          link: 'PCB线宽与电流的关系',
-        },
-        {
-          link: '函数思想在电路设计中的应用',
-        },
-        {
-          link: '基本元器件选型',
-        },
-        {
-          link: '锂电池的选型',
-        },
-        {
-          link: '电源的设计',
-        },
-        {
-          link: 'AD小技巧整理',
-        },
-        {
-          link: 'AD入门系列一：环境搭建',
-        },
-        {
-          link: 'AD入门系列二：基础知识',
-        },
-        {
-          link: 'AD入门系列三：原理图绘制',
-        },
-        {
-          link: 'AD入门系列四：PCB绘制',
-        },
-        {
-          link: 'AD入门系列五：库文件绘制',
-        },
-        {
-          link: '自制CMSIS-DAP',
-        },
-        {
-          link: 'PCB名片设计',
-        },
-        {
-          link: '常用的防反接电路',
-        },
-        {
-          link: '什么是金手指',
-        },
-        {
-          link: '电路设计中常用的接口及关键点',
-        },
-        {
-          link: 'AD使用Git的注意事项',
-        },
+         {title: '1', link: '/AirForce-充满灵性的电机驱动模块',},
+         {title: '1', link: '/如何设计一款单片机的最小系统',},
+         {title: '1', link: '/SwiftCtrl-蓝牙手柄',},
+         {title: '1', link: '/TinyDVR-小巧身材，满载动力',},
+         {title: '1', link: '/ZenDriver-高性能的电机驱动',},
+         {title: '1', link: '/PCB布线规范',},
+         {title: '1', link: '/PCB元件布局规范',},
+         {title: '1', link: '/PCB线宽与电流的关系',},
+         {title: '1', link: '/函数思想在电路设计中的应用',},
+         {title: '1', link: '/基本元器件选型',},
+         {title: '1', link: '/锂电池的选型',},
+         {title: '1', link: '/电源的设计',},
+         {title: '1', link: '/AD小技巧整理',},
+         {title: '1', link: '/AD入门系列一：环境搭建',},
+         {title: '1', link: '/AD入门系列二：基础知识',},
+         {title: '1', link: '/AD入门系列三：原理图绘制',},
+         {title: '1', link: '/AD入门系列四：PCB绘制',},
+         {title: '1', link: '/AD入门系列五：库文件绘制',},
+         {title: '1', link: '/自制CMSIS-DAP',},
+         {title: '1', link: '/PCB名片设计',},
+         {title: '1', link: '/常用的防反接电路',},
+         {title: '1', link: '/什么是金手指',},
+         {title: '1', link: '/电路设计中常用的接口及关键点',},
+         {title: '1', link: '/AD使用Git的注意事项',},
 
-      ]
-    },
+    ]
+  },
 
   ]
 })
